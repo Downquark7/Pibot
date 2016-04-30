4371f6fd56d3a18b4892fe404ef949eeba82638a
Downquark7 <mail2emn@gmail.com>
2016-04-29 23:34:34 -0500

    Update pibot.py

--- pibot.py
+++ pibot.py
@@ -153,7 +153,7 @@ while True:
                 side = 0
             else:
                 side = 1
-            L_motor_speed=(50+direction*turning_rate/w)
+            L_motor_speed=(50-direction*turning_rate/w)
             R_motor_speed=(50+direction*turning_rate/w)
             print("found; direction=",direction,"turning_rate",turning_rate,"w",w)
             
