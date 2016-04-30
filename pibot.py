79c701d7dad3c1321bc166ca2b7015d1b5dfb51c
Downquark7 <mail2emn@gmail.com>
2016-04-29 23:27:33 -0500

    Update pibot.py

--- pibot.py
+++ pibot.py
@@ -15,7 +15,7 @@ import threading
 side = 0
 w=80
 h=60
-turning_rate = 60
+turning_rate = 1
 running = True
 L_motor_speed=0
 R_motor_speed=0
@@ -155,7 +155,7 @@ while True:
                 side = 1
             L_motor_speed=(direction*turning_rate/w)
             R_motor_speed=(direction*turning_rate/w)
-            print("found")
+            print("found; direction=",direction,"turning_rate",turning_rate,"w",w)
             
         found = False
         print("camera update")
