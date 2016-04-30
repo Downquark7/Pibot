d6929ae92d43dd30572908b9b30fe7d5cad20df2
Downquark7 <mail2emn@gmail.com>
2016-04-29 22:47:52 -0500

    Update pibot.py

--- pibot.py
+++ pibot.py
@@ -15,7 +15,7 @@ import threading
 side = 0
 w=80
 h=60
-turning_rate = 60
+turning_rate = 30
 running = True
 L_motor_speed=0
 R_motor_speed=0
@@ -158,6 +158,7 @@ while True:
             
             
         found = False
+        print("camera update")
     except KeyboardInterrupt:
         break
     key_pressed = cv2.waitKey(33)
