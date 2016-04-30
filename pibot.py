79d09cbde3286e33400d112f21c2d0e173ac4b27
Downquark7 <mail2emn@gmail.com>
2016-04-29 22:39:23 -0500

    Update pibot.py

--- pibot.py
+++ pibot.py
@@ -67,6 +67,7 @@ class MotorThread(threading.Thread):
             self.b_motor.run_forever(duty_cycle_sp = dc_clamp(scalestick(L_motor_speed)))
             self.c_motor.run_forever(duty_cycle_sp = dc_clamp(scalestick(R_motor_speed)))
             #self.d_motor.run_forever(duty_cycle_sp = dc_clamp(other_speed))
+            time.sleep(0.2)
             print("motor update: ",dc_clamp(scalestick(L_motor_speed))," | ",dc_clamp(scalestick(R_motor_speed)))
             print("motor_speeds: ",L_motor_speed," | ",R_motor_speed)
         
