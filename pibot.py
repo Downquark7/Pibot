ee7333938b4db22a1f4c38c805f5eae644340298
Downquark7 <mail2emn@gmail.com>
2016-05-02 19:59:29 -0500

    Update pibot.py

--- pibot.py
+++ pibot.py
@@ -25,8 +25,8 @@ speed_80 = 0
 forward_speed = 0
 running = True
 porportional_gain = 0.3
-intergral_gain = 0.01
-derivative_gain = 0.1
+intergral_gain = 0#.01
+derivative_gain = 0#.1
 
 intergral = 0
 previous_error = 0
