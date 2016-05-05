db31b93cd102279a4a4ef1091bb15957c4d59b02
Downquark7 <mail2emn@gmail.com>
2016-05-05 16:31:01 -0500

    Update nxtbot.py

--- nxtbot.py
+++ nxtbot.py
@@ -186,11 +186,11 @@ while True:
             
             #forward_speed = ((h - (boxx + boxh + (h / 4))) * y_gain) + (area_gain * ((h*w*0.5) - area) / (h*w))
             forward_speed = fwd_speed
-            if boxx+boxh > h*0.7:
+            if boxy+boxh > h*0.7:
                 forward_speed=0
             if area > target_area*0.9:
                 forward_speed=0
-            if boxx+boxh > h*0.8:
+            if boxy+boxh > h*0.8:
                 forward_speed = -(fwd_speed)
             if area > target_area*1.1:
                 forward_speed = -(fwd_speed)
