ec5d05e8c6b34175babd27784940205933fa4189
Downquark7 <mail2emn@gmail.com>
2016-05-23 21:37:43 -0500

    Update nxtbot2.0.py

--- nxtbot2.0.py
+++ nxtbot2.0.py
@@ -172,8 +172,8 @@ while True:
             turn_error = (direction * turn_gain)
             accumulated_turn = accumulated_turn + (accumulated_gain * direction)
             #print ((((area/(h*w)) * direction*turn_gain) * 2) / w), (accumulated_turn*2)/w, (-turn_error*2)/w
-            L_motor_speed = forward_speed + (direction * turn_gain + accumulated_turn - turn_error)
-            R_motor_speed = forward_speed - (direction * turn_gain + accumulated_turn - turn_error)
+            L_motor_speed = forward_speed + (direction)# * turn_gain + accumulated_turn - turn_error)
+            R_motor_speed = forward_speed - (direction)# * turn_gain + accumulated_turn - turn_error)
             print L_motor_speed, R_motor_speed, direction
             if not move:
                 print L_motor_speed, R_motor_speed, boxy+boxh, h*0.8
