17321141734b246a65b2621fb2c3980d426699bb
Downquark7 <mail2emn@gmail.com>
2016-05-23 20:50:47 -0500

    Update nxtbot2.0.py

--- nxtbot2.0.py
+++ nxtbot2.0.py
@@ -30,10 +30,6 @@ import time
 #import time
 
 
-h_tolerance = 10
-s_tolerance = 30
-v_tolerance = 30
-
 side = 0
 L_motor_speed = 0
 R_motor_speed = 0
@@ -54,12 +50,10 @@ fwd_speed = input("fwd speed")
 search_speed = input("search speed")
 #y_gain = input("y gain")
 #area_gain = input("area gain")
-target_diam = input("object diameter in inches")
-turn_gain = 20000/target_diam
-target_area = (((3.14*(target_diam/2)*(target_diam/2))/500)*h*w)
+turn_gain = 20
 y_target = h
-overturn_gain = 10000.0
-accumulated_gain = 0.2
+overturn_gain = 0
+accumulated_gain = 0
 turn_error = 0
 accumulated_turn = 0
 diam = 0
@@ -90,8 +84,7 @@ def scalestick(value):
 def dc_clamp(value):
     return clamp(value,(-100,100))
 
-def ac_clamp(value):
-    return clamp(value,(0,255))
+
 
 #left_speed = 0
 #right_speed = 0
@@ -99,75 +92,6 @@ def ac_clamp(value):
 #other_speed = 0
 
 
-
-target=[100,100,100]
-
-
-while (True):
-    try:
-        success, image = cap.read()
-        #h, w, channels = image.shape
-        #image = cv2.flip(image,-1)
-        
-        binary = cv2.GaussianBlur(image,(5,5),0)
-        binary = cv2.cvtColor(binary,cv2.COLOR_BGR2HSV)
-        lower_pink = np.uint8([ac_clamp(target[0]-h_tolerance),ac_clamp(target[1]-s_tolerance),ac_clamp(target[2]-v_tolerance)])
-        upper_pink = np.uint8([ac_clamp(target[0]+h_tolerance),ac_clamp(target[1]+s_tolerance),ac_clamp(target[2]+v_tolerance)])
-        kernel = np.ones((5,5),np.uint8)
-        mask = cv2.inRange(binary,lower_pink,upper_pink)
-        mask = cv2.erode(mask,kernel,iterations=1)
-        mask = cv2.dilate(mask,kernel,iterations=1)
-        contours, hierarchy = cv2.findContours(mask,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-        blob_x = w/2
-        area = 0
-        image = cv2.GaussianBlur(image,(5,5),0)
-        color = map(int,image[h/2][w/2])
-        image_HSV = cv2.cvtColor(image,cv2.COLOR_BGR2HSV)
-        target=image_HSV[h/2][w/2]
-        colour = str(image_HSV[h/2][w/2])
-        if len(contours)>0:
-            largest = 0
-            area = cv2.contourArea(contours[0])
-            if len(contours)>1:
-                for i in range(1,len(contours)):
-                    temp_area = cv2.contourArea(contours[i])
-                    if temp_area>area:
-                        area=temp_area
-                        largest = i
-            if area > 100:
-                found=True
-                coords = cv2.moments(contours[largest])
-                blob_x = int(coords['m10']/coords['m00'])
-                blob_y = int(coords['m01']/coords['m00'])
-                diam = int(np.sqrt(area)/4)
-                if visuals:
-                    cv2.circle(image,(blob_x,blob_y),diam,(0,255,0),1)
-                    #cv2.putText(image,str(diam),(10,30),cv2.FONT_HERSHEY_PLAIN,1,[255,255,255])
-                    cv2.line(image,(blob_x-2*diam,blob_y),(blob_x+2*diam,blob_y),(0,255,0),1)
-                    cv2.line(image,(blob_x,blob_y-2*diam),(blob_x,blob_y+2*diam),(0,255,0),1)
-                boxx,boxy,boxw,boxh = cv2.boundingRect(contours[largest])
-                if visuals:
-                    cv2.rectangle(image,(boxx,boxy),(boxx+boxw,boxy+boxh),(0,0,255),2)
-            if visuals:
-                cv2.drawContours(image,contours,largest,(255,0,0),3)
-                
-        if visuals:
-            cv2.line(image,(0,h/2),(w-1,h/2),color)        
-            cv2.line(image,(w/2,0),(w/2,h-1),color)
-            cv2.putText(image,colour,(10,30),cv2.FONT_HERSHEY_PLAIN,1,[255,255,255])
-            cv2.imshow('View',image)
-        else:
-            print(colour, diam)
-        # Esc key to stop, otherwise repeat after 1 milliseconds
-        key_pressed = cv2.waitKey(33)
-        if key_pressed == 27:
-            break
-    except KeyboardInterrupt:
-        break
-
-target=image_HSV[h/2][w/2]
-print target
-time.sleep(2)
 
 # Main loop
 while True:
@@ -181,8 +105,8 @@ while True:
         #image2 = cv2.cvtColor(image2,cv2.COLOR_RGB2BGR)
         binary = cv2.GaussianBlur(image,(5,5),0)
         binary = cv2.cvtColor(binary,cv2.COLOR_BGR2HSV)
-        lower_pink = np.uint8([ac_clamp(target[0]-h_tolerance),ac_clamp(target[1]-s_tolerance),ac_clamp(target[2]-v_tolerance)])
-        upper_pink = np.uint8([ac_clamp(target[0]+h_tolerance),ac_clamp(target[1]+s_tolerance),ac_clamp(target[2]+v_tolerance)])
+        lower_pink = np.uint8([0,0,200])
+        upper_pink = np.uint8([255,30,255])
         kernel = np.ones((5,5),np.uint8)
         mask = cv2.inRange(binary,lower_pink,upper_pink)
         mask = cv2.erode(mask,kernel,iterations=1)
@@ -242,19 +166,15 @@ while True:
             forward_speed = fwd_speed
             if boxy+boxh > h*0.8:
                 forward_speed=0
-            if area > target_area*0.9:
-                forward_speed=0
             if boxy+boxh > h*0.9:
                 forward_speed = -(fwd_speed)
-            if area > target_area*1.1:
-                forward_speed = -(fwd_speed)
-            turn_error = (direction * turn_gain) / w
-            accumulated_turn = accumulated_turn + (accumulated_gain * direction * turn_gain * 2) / w
+            turn_error = (direction * turn_gain)
+            accumulated_turn = accumulated_turn + (accumulated_gain * direction)
             #print ((((area/(h*w)) * direction*turn_gain) * 2) / w), (accumulated_turn*2)/w, (-turn_error*2)/w
-            L_motor_speed = forward_speed + ((((area/(h*w)) * direction * turn_gain + accumulated_turn - turn_error) * 2) / w)
-            R_motor_speed = forward_speed - ((((area/(h*w)) * direction * turn_gain + accumulated_turn - turn_error) * 2) / w)
+            L_motor_speed = forward_speed + (direction * turn_gain + accumulated_turn - turn_error)
+            R_motor_speed = forward_speed - (direction * turn_gain + accumulated_turn - turn_error)
             if not move:
-                print L_motor_speed, R_motor_speed, area, target_area*0.9, boxy+boxh, h*0.8
+                print L_motor_speed, R_motor_speed, boxy+boxh, h*0.8
                 time.sleep(0.1)
             if abs(L_motor_speed) < 10:
                 L_motor_speed = 0
